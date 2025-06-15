034c524cb9a127fb29894fa919beeb9b90a71175
Marcos Daniel Romero Garcia <Marck Romero>
2025-06-15 00:42:03 -0500

    Proyecto incializado

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET() { 
+
+  return NextResponse.json({
+    message: 'Hello World'
+  })
+}
+
+
+export async function POST(request: Request) { 
+    return NextResponse.json({
+        message: 'Hello World',
+        method: request.method
+    })
+}
