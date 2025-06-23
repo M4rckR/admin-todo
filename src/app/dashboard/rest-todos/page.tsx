63e88fe70016cdd22c0d83ad5a49c353534af0a7
Marcos Daniel Romero Garcia <Marck Romero>
2025-06-22 22:20:09 -0500

    Seccion pausada, me quedo jato

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,7 +1,25 @@
-export default function RestTodos() {
+// import { useEffect } from "react";
+
+import { prisma } from "@/lib/prisma";
+
+export default async function RestTodos() {
+  const todos = await prisma.todo.findMany({orderBy: {description: "asc"}});
+
+  // useEffect(() => {
+  //   fetch('/api/todos')
+  //   .then(res => res.json())
+  //   .then(console.log)
+  // }, [])
+
   return (
-    <div>
-      <h1>RestTodos Page</h1>
-    </div>
-  )
-}   
+      // <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      //   <WidgetItem />
+      // </div>
+      <>
+        <h1>Rest Todos</h1>
+        {
+          JSON.stringify(todos)
+        }
+      </>
+  );
+}
---
+++ src/components/SidebarItem.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { usePathname } from "next/navigation";
+type SidebarItemProps = {
+  icon: React.ReactNode;
+  path: string;
+  title: string;
+}
+
+export const SidebarItem = ({ icon, path, title }: SidebarItemProps) => {
+  const pathname = usePathname();
+
+
+  return (
+    <li>
+    <a
+      href="#"
+      className={`relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400 ${pathname === path} ? "text-white bg-gradient-to-r from-sky-600 to-cyan-400" : ""`}
+    >
+      {icon}
+      <span className="-mr-1 font-medium">{title}</span>
+    </a>
+  </li>
+  )
+}
